daed20255488bc82eb7fefccd73698e16232f465
jasper9w <jasper9w@hotmail.com>
2024-10-10 01:30:58 +0800

    config: Import Tailwind CSS

--- src/main.js
+++ src/main.js
@@ -4,4 +4,7 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 
+// 导入 Tailwind CSS
+import './index.css'
+
 createApp(App).mount('#app')
